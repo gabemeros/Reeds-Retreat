cb4fce1635130ec9e22c275820fbd7e9062b0e8a
gabemeros <gmeros@kent.edu>
2024-05-04 16:03:13 -0400

    Update submit_booking.php

--- booking/submit_booking.php
+++ booking/submit_booking.php
@@ -6,11 +6,10 @@ $email = $_POST['email'];
 $phone = $_POST['phone'];
 $start_date = $_POST['start_date'];
 $end_date = $_POST['end_date'];
-$message = $_POST['message'];
 $cabin_id = $_POST['cabin_id'];
 
 $sql = "INSERT INTO Cabin_Booking (customer_id, cabin_id, start_date, end_date, message) 
-        VALUES ('$name', '$email', '$phone', '$start_date', '$end_date', '$message')";
+        VALUES ('$name', '$email', '$phone', '$start_date', '$end_date')";
 
 if ($conn->query($sql) === TRUE) {
     echo "Booking successfully submitted!";
