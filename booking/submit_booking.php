3c93602047f17cdaff2c4e34aa71b0deedbd2e3e
gabemeros <gmeros@kent.edu>
2024-05-07 22:45:18 -0400

    updated booking
    updated booking.php and the html to pull user input and match fields from the database

--- booking/submit_booking.php
+++ booking/submit_booking.php
@@ -6,10 +6,11 @@ $email = $_POST['email'];
 $phone = $_POST['phone'];
 $start_date = $_POST['start_date'];
 $end_date = $_POST['end_date'];
-$cabin_id = $_POST['cabin_id'];
+$cabin_id = $_POST['cabin'];
+$event_id = $_POST['event'];
 
-$sql = "INSERT INTO customer_booking (customer_id, cabin_id, start_date, end_date) 
-        VALUES ('$name', '$email', '$phone', '$start_date', '$end_date')";
+$sql = "INSERT INTO customer_booking (name, email, phone, start_date, end_date, cabin, event) 
+        VALUES ('$name', '$email', '$phone', '$start_date', '$end_date', '$cabin_id', '$event_id')";
 
 if ($conn->query($sql) === TRUE) {
     echo "Booking successfully submitted!";
